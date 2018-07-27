5d30a85e7a449639b71db1504303c88276438787
TheEndurance <rawa.jalal@gmail.com>
2018-07-26 22:00:47 -0400

    click to make points

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Point from './Components/Point';
+
+
+const svg = document.getElementById('svg');
+const DrawEnum = {
+  "point":1
+};
+Object.freeze(DrawEnum);
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      points: [],
+      mode: DrawEnum.point,
+    };
+    this.myRef = React.createRef();
+    this.mouseMatrixTransformation = this.mouseMatrixTransformation.bind(this);
+    this.draw = this.draw.bind(this);
+  }
+
+  mouseMatrixTransformation(event){
+    let point = this.myRef.current.createSVGPoint();
+    point.x = event.clientX;
+    point.y = event.clientY;
+    return point.matrixTransform(this.myRef.current.getScreenCTM().inverse());
+  }
+
+  draw(event){
+    let clickPoint = this.mouseMatrixTransformation(event);
+    if (this.state.mode === DrawEnum.point){
+      let point = <Point x={clickPoint.x} y={clickPoint.y}/>
+      this.setState((prevState)=>{
+        return { points: [...prevState.points,point]};
+      });
+    }
+  }
+  render() {
+    const { points } = this.state;
+    return (
+      <svg ref={this.myRef} onClick={this.draw} id="svg"  version="1.1" baseProfile="full" width="1920" height="1080" xmlns="http://www.w3.org/2000/svg">
+      {points}
+      </svg>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Point.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Point = (props) => {
+    return (
+        <circle cx={props.x} cy={props.y} r="5">
+        </circle>
+    )
+}
+
+export default Point;
