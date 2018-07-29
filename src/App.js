94d456ceb71625ce8a5044c92f5f9caf42bf3290
TheEndurance <rawa.jalal@gmail.com>
2018-07-29 11:19:59 -0400

    begin drawing polygons

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Point from './Components/Point';
 
 const svg = document.getElementById('svg');
 const DrawEnum = {
-  "point":1
+  "none":1,
+  "polygon":2
 };
 Object.freeze(DrawEnum);
 
@@ -15,24 +16,27 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      tempPolygon: null,
+      clickedPoints: [],
       points: [],
-      mode: DrawEnum.point,
+      mode: DrawEnum.polygon,
     };
     this.myRef = React.createRef();
     this.mouseMatrixTransformation = this.mouseMatrixTransformation.bind(this);
     this.draw = this.draw.bind(this);
+    this.canvasClicked = this.canvasClicked.bind(this);
   }
 
-  mouseMatrixTransformation(event){
+  mouseMatrixTransformation(x,y){
     let point = this.myRef.current.createSVGPoint();
-    point.x = event.clientX;
-    point.y = event.clientY;
+    point.x = x;
+    point.y = y;
     return point.matrixTransform(this.myRef.current.getScreenCTM().inverse());
   }
 
   draw(event){
     event.preventDefault();
-    let clickPoint = this.mouseMatrixTransformation(event);
+    let clickPoint = this.mouseMatrixTransformation(event.clientX,event.clientY);
     if (this.state.mode === DrawEnum.point){
       let point = <Point x={clickPoint.x} y={clickPoint.y}/>
       this.setState((prevState)=>{
@@ -40,14 +44,35 @@ class App extends Component {
       });
     }
   }
+
+  canvasClicked(event){
+    event.preventDefault();
+    let clickPoint = this.mouseMatrixTransformation(event.clientX,event.clientY);
+    this.setState((prevState)=>{
+      return { clickedPoints: [...prevState.clickedPoints, {x: clickPoint.x, y:clickPoint.y}]};
+    });
+
+    
+    const points = arrayPointsToString(this.state.clickedPoints);
+    this.setState({
+      tempPolygon : (<polygon points={points} style={{fill:"white",stroke:"purple"}}/>)
+    });
+  }
   render() {
-    const { points } = this.state;
+    const { points, tempPolygon } = this.state;
     return (
-      <svg ref={this.myRef} onClick={this.draw} id="svg"  version="1.1" baseProfile="full" width="1920" height="1080" xmlns="http://www.w3.org/2000/svg">
-      {points}
+      <svg ref={this.myRef} onClick={this.canvasClicked} id="svg"  version="1.1" baseProfile="full" width="1920" height="1080" xmlns="http://www.w3.org/2000/svg">
+        {tempPolygon}
       </svg>
     );
   }
 }
 
 export default App;
+
+function arrayPointsToString(clickedPoints) {
+  return clickedPoints.reduce((accumulator, current) => {
+    return accumulator += `${current.x},${current.y} `;
+  },"");
+}
+
